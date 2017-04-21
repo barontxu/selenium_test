217378e4383aece555754e1d3a85d753384a1f79
Zizhe Xu <zzhxu@mobvoi.com>
2017-04-21 14:24:29 +0800

    first commit

---
+++ tests/test_base.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
+class TestBase(object):
+    def __init__(self):
+        pass
+
+    def run_test():
+        pass
